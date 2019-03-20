1de8549880af91394700ecb04168afe05585c2f7
Sarath Sattiraju <sarath.sattiraju@gmail.com>
2019-03-20 07:16:23 +0530

    Removed files.

--- server/config.js
+++
@@ -1,7 +0,0 @@
-PORT = 9000;
-DATABASE_URI = "mongodb://root:AIzaSyDc613XSx9_lZTv7qNFCxEhwiRHRXYmCNE@ds135540.mlab.com:35540/chat";
-
-module.exports = {
-    "PORT": PORT,
-    "DATABASE_URI": DATABASE_URI
-};
--- src/config.js
+++
@@ -1,6 +0,0 @@
-const CLIENT_ID="195610010161-nnmg2nhcaoterfsdfqu7ubh1rd7jhl3s.apps.googleusercontent.com";
-const API_URL="http://localhost:9000/";
-
-export {
-    CLIENT_ID, API_URL
-}
